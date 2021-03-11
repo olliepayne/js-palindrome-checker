4486eddad026b9424b00eaa9aa8cbad037f6455f
olliepayne <olliepayne@me.com>
2021-03-11 13:58:53 -0700

    tweak

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const isPalindrome = (str) => {
   const validChars = 
     [
       'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-      't', 'u', 'v', 'w', 'x', 'y', 'z'
+      't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'
     ]
 
   let original = str.toLowerCase()
@@ -13,12 +13,16 @@ const isPalindrome = (str) => {
   for(const char in original) {
     if(validChars.includes(original[char])) copy += original[char]
   }
-  original = copy
   
-  let reversed = original.split("").reverse().join("")
+  if(copy.length > 0) {
+    original = copy 
+    let reversed = original.split("").reverse().join("")
 
-  for(const char in original) {
-    if(original[char] !== reversed[char]) return false
+    for(const char in original) {
+      if(original[char] !== reversed[char]) return false
+    }
+  } else {
+    return false
   }
 
   return true
